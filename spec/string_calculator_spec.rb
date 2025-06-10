d4e6fd22859cf11daa16d57d6a39cdf60bd5629d
Varsha <varshachawla042@gmail.com>
2025-06-10 15:15:57 +0530

    Added test case for single number as input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,9 +2,14 @@ require 'string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
+    let(:calculator) { StringCalculator.new }
+    
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number itself when a single number is passed' do
+      expect(calculator.add("1")).to eq(1)
     end
   end
 end
