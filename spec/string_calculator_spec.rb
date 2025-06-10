c7bd40e29398ee4d87f39cb3a94d04d71a72cf12
Varsha <varshachawla042@gmail.com>
2025-06-10 15:42:31 +0530

    Added test case for two comma separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,13 +3,17 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
   describe '#add' do
     let(:calculator) { StringCalculator.new }
-    
+
     it 'returns 0 for an empty string' do
       expect(calculator.add("")).to eq(0)
     end
 
     it 'returns the number itself when a single number is passed' do
       expect(calculator.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two comma-separated numbers' do
+      expect(calculator.add("2,3")).to eq(5)
     end
   end
 end
