296ecba60c34d37df9d4054349f6dc3208a1063b
Varsha <varshachawla042@gmail.com>
2025-06-11 12:52:09 +0530

    Added test case to support different delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -31,5 +31,9 @@ RSpec.describe StringCalculator do
     it 'returns the sum of numbers separated by commas and newlines' do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
+    
+    it 'returns the sum of numbers with a custom delimiter' do
+      expect(calculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
