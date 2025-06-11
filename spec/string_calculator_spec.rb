36248f1d763265d32d853280aa032613b08601e7
Varsha <varshachawla042@gmail.com>
2025-06-11 13:09:12 +0530

    Added test case for negative number as input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1,2,3")).to eq(6)
     end
 
-    it 'handles negative numbers' do
-      expect(calculator.add("1,-2,3")).to eq(2)
+    it 'raises an exception when negative numbers are included' do
+      expect { calculator.add("1,-2,-5,3") }.to raise_error("negative numbers not allowed -2,-5")
     end
 
     it 'handles large numbers' do
@@ -31,7 +31,7 @@ RSpec.describe StringCalculator do
     it 'returns the sum of numbers separated by commas and newlines' do
       expect(calculator.add("1\n2,3")).to eq(6)
     end
-    
+
     it 'returns the sum of numbers with a custom delimiter' do
       expect(calculator.add("//;\n1;2")).to eq(3)
     end
